b97177db46c91bc93b77cfca31b33e6143b6cf72
xavigg <xavigg@users.noreply.github.com>
2023-04-12 20:02:58 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-      <><h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1><p className="text-2xl ...">The quick brown fox ...</p></>
+      <>
+      <h1 className="text-3xl font-bold underline">COMPRAMOS DOLARES 💵 MEJOR COTIZACION</h1>
+      </>
     )
   }
